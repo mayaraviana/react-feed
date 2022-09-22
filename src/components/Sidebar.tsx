eede0810a6b866224edd065de9bf500fb1cda7b6
mayaraviana <mayaraviana60@hotmail.com>
2022-09-22 10:38:45 -0300

    Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,29 @@
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1576836165612-8bc9b07e7778?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=50"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/mayaraviana.png" />
+
+        <strong>Mayara Viana</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Edit profile
+        </a>
+      </footer>
+    </aside>
+  );
+}
