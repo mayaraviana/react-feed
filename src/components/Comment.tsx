19e353e0ca3ba1dead188f6d67c63b51a1e29baa
mayaraviana <mayaraviana60@hotmail.com>
2022-09-22 10:38:21 -0300

    Comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import styles from "./Comment.module.css";
+
+import { ThumbsUp, Trash } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        hasBorder={false}
+        src="https://github.com/mayaraviana.png"
+        alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Mayara Viana</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+                About 1h ago
+              </time>
+            </div>
+
+            <button onClick={handleDeleteComment} title="Delete comment">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Like <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
